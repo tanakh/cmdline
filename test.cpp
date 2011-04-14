030b2dacc6a0f8dd19d637c1e588e6d7b2c51418
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2011-04-15 04:00:00 +0900

    improve document.

--- test.cpp
+++ test.cpp
@@ -36,15 +36,15 @@ int main(int argc, char *argv[])
   a.add<string>("host", 'h', "host name", true, "");
   a.add<int>("port", 'p', "port number", false, 80, cmdline::range(1, 65535));
   a.add<string>("type", 't', "protocol type", false, "http", cmdline::oneof<string>("http", "https", "ssh", "ftp"));
-  a.footer("filename ...");
-  a.set_program_name("test");
+  a.add("gzip", '\0', "gzip when transfer");
 
   a.parse_check(argc, argv);
 
-  cout<<a.get<string>("host")<<":"<<a.get<int>("port")<<endl;
+  cout << a.get<string>("type") << "://"
+       << a.get<string>("host") << ":"
+       << a.get<int>("port") << endl;
 
-  for (size_t i=0; i<a.rest().size(); i++)
-    cout<<"- "<<a.rest()[i]<<endl;
+  if (a.exist("gzip")) cout << "gzip" << endl;
 
   return 0;
 }
